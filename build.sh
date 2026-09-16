a0e8e82dad8d696caae30413e2f87dd4193cf44f
Manchick <manchicking@gmail.com>
2026-09-16 20:43:59 +0200

    Fix build.sh

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ VERSION="3.1.0"
 rm *.zip
 echo "{
     \"pack\": {
-        \"description\": \"§o*Room plants for dummies*\n§8@Manchick \| v§7$VERSION\",
+        \"description\": \"§o*Room plants for dummies*\n§8@Manchick | v§7$VERSION\",
         \"max_format\": [121, 0],
         \"min_format\": [121, 0]
     }
